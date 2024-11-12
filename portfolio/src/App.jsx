705891823020561e1d9538abfcf9ca40eb6f0170
Stephon Stokley <79936294+TheLonerCoder@users.noreply.github.com>
2024-11-11 22:06:13 -0600

    Basic BIO layout complete

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Bio from './components/Bio'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>The Loner Coder</h1>
+      {/* <h1>The Loner Coder</h1> */}
+      <Bio />
     </>
   )
 }
---
+++ portfolio/src/components/Bio.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+
+
+
+// Web Developer, Entrepreneur, Tech-enthuesist
+
+function Bio() {
+
+  const [name, changeName] = useState('Stephon Stokley');
+  const [underName, setUnderName] = useState('Web Developer');
+
+  // ? Change name on hover
+  let nameChange = () => {
+    changeName('The Loner Coder');
+  }
+
+  let nameChangeBack = () => {
+    changeName('Stephon Stokley');
+  }
+
+  return (
+    <div>
+      <h1 onMouseEnter={nameChange} onMouseLeave={nameChangeBack}>Hello! I'm {name}</h1>
+      <h4>I'm a {underName}</h4>
+
+      <p>I'm a React-focused Web Developer (MERN-Stack) with some experience in Python. I got my first taste of code with a 3 month Android App development Bootcamp back in 2019 but recently received my certification from Nucamp (for Full Stack Web Development).</p>
+
+      <p>My projects are passion-focused, pertaining to immersive technologies, exoplanets/space, entrepreneurship, learning languages, and music.</p>
+
+      <div>
+        <div>
+          <a href="">Git</a>
+          <a href="">YT</a>
+          <a href="">IN</a>
+        </div>
+
+        <div>
+          <b>Email Me</b>
+          <b>Resume</b>
+          <b>Projects</b>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Bio;
